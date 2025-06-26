a2e683de569c7192edd0d6a522f1841235c74018
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-06-26 13:48:16 -0300

    metodo para limpar formulário

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -13,15 +13,15 @@ export class Negociacao {
 
     //pegando componentes privados
 
-    get data(){
+    get data(): Date{
         return this._data;
     }
 
-    get quantidade(){
+    get quantidade(): number{
         return this._quantidade;
     }
 
-    get valor(){
+    get valor(): number{
         return this._valor;
     }
 
